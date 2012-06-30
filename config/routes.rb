8b33287cc6c51774d42a6f8462554515cd8f3b27
Jordan Scales <scalesjordan@gmail.com>
2012-06-30 01:37:34 -0400

    removed unnecessary resources for ratings controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Friendratings::Application.routes.draw do
   
   resources :pages
-  resources :ratings
+  post 'ratings/:id' => 'ratings#create', :as => 'ratings'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
