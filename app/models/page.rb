cc20011f628308178dff2830a3c531f4fffcdb47
Jordan Scales <scalesjordan@gmail.com>
2012-06-30 00:11:42 -0400

    model relationships

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,3 +1,5 @@
 class Page < ActiveRecord::Base
   attr_accessible :author, :description, :name, :reported
+
+  has_many :ratings, :dependent => :destroy
 end
--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,3 +1,5 @@
 class Rating < ActiveRecord::Base
   attr_accessible :content, :name, :page_id, :rating, :reported, :summary
+
+  belongs_to :page
 end
