32dc3c0f0d696e0167640a4aefee9b8695fa2185
Jordan Scales <scalesjordan@gmail.com>
2012-06-30 01:37:22 -0400

    removed bad migration

--- db/migrate/20120630052813_change_rating_precision.rb
+++
@@ -1,7 +0,0 @@
-class ChangeRatingPrecision < ActiveRecord::Migration
-  def change
-    change_table :ratings do |t|
-      t.change :rating, :decimal, :precision => 2, :scale => 2
-    end
-  end
-end
