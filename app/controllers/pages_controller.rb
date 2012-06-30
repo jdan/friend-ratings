8f7f116ac63457c5dcb8ea642a4ee05e87ddbf56
Jordan Scales <scalesjordan@gmail.com>
2012-06-30 00:03:42 -0400

    pages controller

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,10 @@
+class PagesController < ApplicationController
+  def new
+  end
+
+  def edit
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Friendratings::Application.routes.draw do
+  get "pages/new"
+
+  get "pages/edit"
+
+  get "pages/show"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
